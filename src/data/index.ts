78c70ec9c1ef82c6eabb220641e24f84aff83b0b
Anees Anwar <anwaranees250@gmail.com>
2021-05-22 17:40:13 +0500

    added Categories

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,154 +1,183 @@
 import { ContentType } from '../components/CardLayout';
-const logo = require('../../public/beauty/beauty.png');
+const beauty = require('../../public/beauty/beauty.png');
+const cloths = require('../../public/clothing/clothing.jpg');
+const jewelry = require('../../public/jewelry/jewelry.jpg');
+const shoes = require('../../public/shoes/shoes.jpg');
+const household = require('../../public/household/household.jpg');
+const kitchen = require('../../public/kitchen/kitchen.jpg');
+const luggage = require('../../public/luggage/luggage.jpg');
+const phones = require('../../public/phones_access/phones_access');
+const health = require('../../public/health/health.jpg');
+const office_products = require('../../public/office-products/office_products');
+const musical_inst = require('../../public/musical_inst/musical_inst.jpg');
+const grocery = require('../../public/grocery/grocery.jpg');
+const handmade = require('../../public/handmade/handmade.jpg');
+const gourmet_food = require('../../public/gourmet_food/gourmet_food.jpg');
+const dining = require('../../public/dining/dining.jpg');
+const appliances = require('../../public/appliances/appliances.jpg');
+const video_games = require('../../public/video_games/video_games.jpg');
+const toys = require('../../public/toys/toys.jpg');
+const tools = require('../../public/tools/tools.jpg');
+const apps_games = require('../../public/apps_games/apps_games.jpg');
+const sports = require('../../public/sports/sports.jpg');
+const sewing = require('../../public/sewing/sewing.jpg');
+const arts_crafts = require('../../public/arts_crafts/arts_crafts.jpg');
+const books = require('../../public/books/books.jpg');
+const cd = require('../../public/cds/cds.jpg');
+const electronics = require('../../public/electronics/electronics.jpg');
+const garden_outdoor = require('../../public/garden_outdoor/garden_outdoor.jpg');
+const industrial = require('../../public/industrial/industrial.jpg');
+const automotive = require('../../public/automotive/automotive.jpg');
+const computers = require('../../public/computers/computers.jpg');
 export const dummyData: ContentType[] = [
     {
         key: 0,
-        name: 'Card 1',
-        value: logo,
+        name: 'Beauty',
+        value: beauty,
     },
     {
         key: 1,
-        name: 'Card 2',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Grocery',
+        value: grocery
     },
     {
         key: 2,
-        name: 'Card 3',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Gourmet Food',
+        value: gourmet_food,
     },
     {
         key: 3,
-        name: 'Card 4',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Computers',
+        value: computers,
     },
     {
         key: 4,
-        name: 'Card 5',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Cell Phones & Accessories',
+        value: phones,
     },
     {
         key: 5,
-        name: 'Card 6',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Electronics',
+        value: electronics,
     },
     {
         key: 6,
-        name: 'Card 7',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'HouseHold',
+        value: household,
     },
     {
         key: 7,
-        name: 'Card 8',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Appliances',
+        value: appliances,
     },
     {
         key: 8,
-        name: 'Card 9',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Toys',
+        value: toys,
     },
     {
         key: 9,
-        name: 'Card 10',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Tools',
+        value: tools,
     },
     {
         key: 10,
-        name: 'Card 11',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Sports',
+        value: sports,
     },
     {
         key: 11,
-        name: 'Card 12',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Video Games',
+        value: video_games,
     },
     {
         key: 12,
-        name: 'Card 13',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Office Products',
+        value: office_products,
     },
     {
         key: 13,
-        name: 'Card 14',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Luggage',
+        value: luggage,
     },
     {
         key: 14,
-        name: 'Card 15',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Kitchen',
+        value: kitchen,
     },
     {
         key: 15,
-        name: 'Card 16',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Dining',
+        value: dining,
     },
     {
         key: 16,
-        name: 'Card 17',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Handmade',
+        value: handmade,
     },
     {
         key: 17,
-        name: 'Card 18',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Garden & Outdoor',
+        value: garden_outdoor,
     },
     {
         key: 18,
-        name: 'Card 19',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Apps and Games',
+        value: apps_games,
     },
     {
         key: 19,
-        name: 'Card 20',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Musical Instruments',
+        value: musical_inst,
     },
     {
         key: 20,
-        name: 'Card 21',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Automotive parts',
+        value: automotive,
     },
     {
         key: 21,
-        name: 'Card 22',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Industrial & Scientific',
+        value: industrial,
     },
     {
         key: 22,
-        name: 'Card 23',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Cloths',
+        value: cloths,
     },
     {
         key: 23,
-        name: 'Card 24',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Shoes',
+        value: shoes,
     },
     {
         key: 24,
-        name: 'Card 25',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Jewelry',
+        value: jewelry,
     },
     {
         key: 25,
-        name: 'Card 26',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Arts & Crafts',
+        value: arts_crafts,
     },
     {
         key: 26,
-        name: 'Card 27',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'Sewing',
+        value: sewing,
     },
     {
         key: 27,
-        name: 'Card 28',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Health',
+        value: health,
     },
     {
         key: 28,
-        name: 'Card 29',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+        name: 'CDs and Vinyls',
+        value: cd,
     },
     {
         key: 29,
-        name: 'Card 30',
-        value: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+        name: 'Books',
+        value: books,
     },
 ];
